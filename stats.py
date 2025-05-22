c175ad9a8088263c69ed7e48b35c8d7011a0e546
Jeremy Garcia <jeremyg@macbookpro.mynetworksettings.com>
2025-05-22 19:07:54 -0400

    Added print report fucntion

--- stats.py
+++ stats.py
@@ -1,20 +1,26 @@
-def get_word_count(filepath):
-    with open(filepath) as f:
-        words = f.read()
-        return len(words.split())
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
-def get_book_text(filepath):
-    with open(filepath) as f:
-        file_contents = f.read()
-        return file_contents
 
-def get_character_count(book_text):
-        characters = {}
-        for c in book_text:
-            lowered = c.lower()
-            if lowered in characters:
-                characters[lowered] += 1
-            else:
-                characters[lowered] = 1
-        return characters
-            
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
